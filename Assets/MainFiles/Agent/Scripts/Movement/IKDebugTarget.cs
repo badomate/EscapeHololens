d82a5ec1f63e75bc0813bb8162e891c0cb851476
Diogo Mendonça <diogo.cunhamen@gmail.com>
2024-01-23 16:16:27 +0000

    Added IK Animation

---
+++ Assets/MainFiles/Agent/Scripts/Movement/IKDebugTarget.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class IKDebugTarget : MonoBehaviour {
+	public IKController iKController;
+
+	[ContextMenu("Set This as Target")]
+	public void SetAsTarget() {
+		iKController.SetTarget(this.transform);
+	}
+	
+	[ContextMenu("Set This as Left Target")]
+	public void SetAsLeftTarget() {
+		iKController.SetTargetLeft(this.transform);
+	}
+
+	[ContextMenu("Set This as Right Target")]
+	public void SetAsRightTarget() {
+		iKController.SetTargetRight(this.transform);
+	}
+}
