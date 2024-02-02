3a43d9b7978a992ee07b55ba0f65a9dc82d463c5
Alex P <alexandra.pato@tecnico.ulisboa.pt>
2024-02-02 20:15:14 +0000

    Twister Manager now uses target to position level

---
+++ Assets/MainFiles/SensorHub/AppRemotingConnector.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.MixedReality.OpenXR.Remoting;
+using UnityEngine;
+
+namespace SensorHub
+{
+    public class AppRemotingConnector: MonoBehaviour
+    {
+        public void Start()
+        {
+            HoloRemoting();
+        }
+
+        public void HoloRemoting()
+        {
+            RemotingConnectConfiguration configuration =
+                new RemotingConnectConfiguration { 
+                    RemotePort = 8265, MaxBitrateKbps = 20000 
+                };
+
+            AppRemoting.StartConnectingToPlayer(configuration);
+        }
+    }
+}
